48b4fceabfc60183f8d80f42156c8278983776b1
Skye J <skye@skyenet.online>
2020-02-11 23:52:45 -0500

    Finalized basic designs for main and projects.

--- run.js
+++ run.js
@@ -1,11 +1,20 @@
 var cook = document.cookie;
 console.log(cook);
 if (cook == ""){
-	setStyleSheetLight();
+  setStyleSheetLight();
 }
 else if (cook =="style=dark"){
-	setStyleSheetDark();
+  setStyleSheetDark();
 }
 else if (cook == "style=light"){
-	setStyleSheetLight();
+  setStyleSheetLight();
 }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
